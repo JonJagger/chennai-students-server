52e6e7c4ecca395412c123e748ce5ad6c7ec5a29
Jon Jagger <jon@jaggersoft.com>
2017-08-04 22:57:59 +0530

    sort image-names on simple route to contrast better with shuffled image-names on shuffled route

--- app/models/images.rb
+++ app/models/images.rb
@@ -5,7 +5,7 @@ class Images
     img_dir = File.expand_path('../public/img', File.dirname(__FILE__))
     Dir.glob("#{img_dir}/*.png").map { |img_file|
       File.basename(img_file, '.png')
-    }
+    }.sort
   end
 
 end
