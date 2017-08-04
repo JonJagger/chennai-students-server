3f7bbf2ca2500b1200cf989a461c32bc03a8529f
Jon Jagger <jon@jaggersoft.com>
2017-08-04 21:21:53 +0530

    added .dockerignore, removed .gitignore

--- build.sh
+++ build.sh
@@ -2,6 +2,3 @@
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 docker-compose --file ${MY_DIR}/docker-compose.yml build
-
-#docker build --tag cyberdojofoundation/chennai_students:1.0  .
-#docker build --no-cache --tag cyberdojofoundation/chennai_students:1.0  .
