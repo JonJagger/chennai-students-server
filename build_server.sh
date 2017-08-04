c62b8db40c99ea6729555d5818a2b9106cc774f5
Jon Jagger <jon@jaggersoft.com>
2017-08-04 22:32:13 +0530

    pull the image in the build-script so its not rebuilt

--- build_server.sh
+++ build_server.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 curl -sSL https://get.docker.com/ | sh
-usermod -aG jrbjagger docker
+docker pull cyberdojo/chennai-students-server:1.0
 curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
 chmod +x /usr/local/bin/docker-compose
 
-#git clone https://github.com/JonJagger/chennai-students-server.git
-#echo
-#echo "$ cd chennai-students-server"
-#echo "$ sudo ./up.sh"
+echo
+echo "$ sudo ./up.sh"
+echo
