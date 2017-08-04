817d159e37f44b3fd224a4b3c1a8e7e24a0a0efc
Jon Jagger <jon@jaggersoft.com>
2017-08-04 22:10:06 +0530

    added script to build server

---
+++ build_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -sSL https://get.docker.com/ | sh
+usermod -aG jrbjagger docker
+curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
+chmod +x /usr/local/bin/docker-compose
+git clone https://github.com/JonJagger/chennai-students-server.git
+
+echo "$ cd chennai-students-server"
+echo "$ ./up.sh"
