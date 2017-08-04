70f208eb6f2b8c65f63bda1344fdefb7407c9878
Jon Jagger <jon@jaggersoft.com>
2017-08-04 22:40:49 +0530

    moved [docker pull] command to end of build-server script so its output can been seen better

--- build_server.sh
+++ build_server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 curl -sSL https://get.docker.com/ | sh
-docker pull cyberdojo/chennai-students-server:1.0
 curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
 chmod +x /usr/local/bin/docker-compose
+docker pull cyberdojo/chennai-students-server:1.0
 
 echo
-echo "$ sudo ./up.sh"
+echo 'sudo ./up.sh'
 echo
