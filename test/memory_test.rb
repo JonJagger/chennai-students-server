c747cf184f33b561044088dbdac14fcb211d524a
Jon Jagger <jon@jaggersoft.com>
2017-08-04 13:42:55 +0530

    test update

--- test/memory_test.rb
+++ test/memory_test.rb
@@ -8,11 +8,28 @@ class MemoryTest < AppTest
 
   def test_get_root
     get '/'
-    assert_equal 200, last_response.status
+    assert_response 200
+  end
+
+  def test_image_index
+    get '/image_index'
+    assert_response 200
+  end
+
+  def test_image_index_shuffled
+    get '/image_index_shuffled'
+    assert_response 200
   end
 
   def test_get_not_found
     get '/not_found'
-    assert_equal 404, last_response.status
+    assert_response 404
   end
+
+  private
+
+  def assert_response(n)
+    assert_equal n, last_response.status
+  end
+
 end
