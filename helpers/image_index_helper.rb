b855a4ec6abb87a21c5e722f65c07d9daf91129d
Jon Jagger <jon@jaggersoft.com>
2017-08-04 12:05:34 +0530

    added all image names to helper ready to be read via ajax call

--- helpers/image_index_helper.rb
+++ helpers/image_index_helper.rb
@@ -3,7 +3,12 @@ module ImageIndexHelper
 
   def image_index
     %w(
-      Akhim Akshaya
+      Akhim Akshaya Arun ArunKumar Ashwin Krishna
+      Deekshita Divya Francis Gayathri Heamanth
+      Madhu Manisha Karthick Mihir Murali
+      Neeharika Prakash Prakhyat Sanjana Sankara
+      Sariha Shravan Sindhura Srikar Surya
+      Tharun Uma Vamshi Veena Vignesh Vinay
     )
   end
 
