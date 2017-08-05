e4fd3853aaa6ef9c39d121d2b2213251ff983a63
Jon Jagger <jon@jaggersoft.com>
2017-08-05 13:17:34 +0530

    added comment to build-server script

--- build_server.sh
+++ build_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Works on Ubuntu 16.04
 curl -sSL https://get.docker.com/ | sh
 curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
 chmod +x /usr/local/bin/docker-compose
