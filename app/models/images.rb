accec0ab6c345474f1bec3e415b79fdccd99051e
Jon Jagger <jon@jaggersoft.com>
2017-08-05 06:40:18 +0530

    show image filename extensions

--- app/models/images.rb
+++ app/models/images.rb
@@ -4,7 +4,7 @@ class Images
   def self.all
     img_dir = File.expand_path('../public/img', File.dirname(__FILE__))
     Dir.glob("#{img_dir}/*.png").map { |img_file|
-      File.basename(img_file, '.png')
+      File.basename(img_file)
     }.sort
   end
 
