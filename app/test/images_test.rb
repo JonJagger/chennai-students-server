4461b64dfbb2481241ec5e3aeaf15a91bae2f96c
Jon Jagger <jon@jaggersoft.com>
2017-08-14 09:54:15 +0530

    added simple test to catch previous img path error

---
+++ app/test/images_test.rb
@@ -0,0 +1,15 @@
+
+require 'minitest/autorun'
+require_relative '../src/images'
+
+class ImagesBase < Minitest::Test
+
+  def test_all_image_filenames_are_collected
+    filenames = Images.filenames
+    assert_equal 34, filenames.size
+    assert_equal 'Akhil.jpg', filenames[0]
+    assert_equal 'Vinay.jpg', filenames[-1]
+    assert filenames.include? 'Surya.png'
+  end
+
+end
