5d1e4790a27f1fe9f77c6c3636fc41cd08980253
Jon Jagger <jon@jaggersoft.com>
2018-07-31 08:43:36 +0100

    ensure up.sh completes

--- sh/up.sh
+++ sh/up.sh
@@ -9,16 +9,16 @@ wait_till_up()
     if docker ps --filter status=running --format '{{.Names}}' | grep -q ^${1}$ ; then
       return
     else
-      sleep 0.5
+      sleep 0.2
     fi
   done
-  echo "${1} not up after 5 seconds"
+  echo "${1} not up after 2 seconds"
   docker logs "${1}"
   exit 1
 }
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 source ${MY_DIR}/../.env
-docker rm -f ${FACES_BOOK_CONTAINER}
+docker rm -f ${FACES_BOOK_CONTAINER} || true
 docker-compose --file ${MY_DIR}/../docker-compose.yml up -d
 wait_till_up ${FACES_BOOK_CONTAINER}
