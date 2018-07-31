06abc43c496edf5e168e02f5342815aaa3498633
Jon Jagger <jon@jaggersoft.com>
2018-07-31 04:06:56 +0100

    added first images of 2018 students

--- app/test/images_test.rb
+++ app/test/images_test.rb
@@ -3,12 +3,9 @@ require_relative '../src/images'
 
 class ImagesBase < Minitest::Test
 
-  def test_all_image_filenames_are_collected
+  def test_image_filenames_are_collected
     filenames = Images.filenames
-    assert_equal 34, filenames.size
-    assert_equal 'Akhil.jpg', filenames[0]
-    assert_equal 'Vinay.jpg', filenames[-1]
-    assert filenames.include? 'Surya.png'
+    assert filenames.size > 0, filenames
   end
 
 end
--- sh/up.sh
+++ sh/up.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 set -e
 
-check_up()
+wait_till_up()
 {
-  set +e
-  local s=$(docker ps --filter status=running --format '{{.Names}}' | grep ^${1}$)
-  set -e
-  if [ "${s}" != "${1}" ]; then
-    echo
-    echo "${1} exited"
-    docker logs ${1}
-    exit 1
-  fi
+  local n=10
+  while [ $(( n -= 1 )) -ge 0 ]
+  do
+    if docker ps --filter status=running --format '{{.Names}}' | grep -q ^${1}$ ; then
+      return
+    else
+      sleep 0.5
+    fi
+  done
+  echo "${1} not up after 5 seconds"
+  docker logs "${1}"
+  exit 1
 }
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 source ${MY_DIR}/../.env
+docker rm -f ${FACES_BOOK_CONTAINER}
 docker-compose --file ${MY_DIR}/../docker-compose.yml up -d
-# crude wait for services
-sleep 1
-check_up ${FACES_BOOK_CONTAINER}
+wait_till_up ${FACES_BOOK_CONTAINER}
